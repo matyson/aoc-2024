7a8cca9f6c851f7a02fcfeb38995148802fee8e0
Matheus Luís <matheus.bernardi@lnls.br>
2024-12-04 13:14:22 -0300

    cleaning up
    1st puzzle was messy

--- 1/main.ts
+++ 1/main.ts
@@ -1,27 +1,3 @@
-const inputPath = "data/input.txt";
-
-const input = await Deno.readTextFile(inputPath);
-
-const leftList: number[] = [];
-const rightList: number[] = [];
-input.split("\n").map((line) => {
-  const [x, y] = line.split("   ").map(Number);
-  leftList.push(x);
-  rightList.push(y);
-});
-
-leftList.sort();
-rightList.sort();
-
-const distances = leftList.map((left, index) => {
-  const right = rightList[index];
-  return Math.abs(left - right);
-});
-
-const totalDistance = sum(distances);
-
-console.log(totalDistance);
-
 function countSimilarity(numbers: number[], target: number): number {
   return numbers.filter((number) => number === target).length * target;
 }
@@ -30,7 +6,38 @@ function sum(numbers: number[]): number {
   return numbers.reduce((acc, number) => acc + number, 0);
 }
 
-const similarityList = leftList.map((left) => countSimilarity(rightList, left));
-const totalSimilarity = sum(similarityList);
+async function main() {
+  const inputPath = "data/input.txt";
 
-console.log(totalSimilarity);
+  const input = await Deno.readTextFile(inputPath);
+
+  const leftList: number[] = [];
+  const rightList: number[] = [];
+  input.split("\n").map((line) => {
+    const [x, y] = line.split("   ").map(Number);
+    leftList.push(x);
+    rightList.push(y);
+  });
+
+  leftList.sort();
+  rightList.sort();
+
+  const distances = leftList.map((left, index) => {
+    const right = rightList[index];
+    return Math.abs(left - right);
+  });
+
+  const totalDistance = sum(distances);
+
+  const similarityList = leftList.map((left) =>
+    countSimilarity(rightList, left)
+  );
+  const totalSimilarity = sum(similarityList);
+
+  console.log("Total distance:", totalDistance);
+  console.log("Total similarity:", totalSimilarity);
+}
+
+if (import.meta.main) {
+  main();
+}
